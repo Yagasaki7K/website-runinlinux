946b4355c4c7a52acd34443303de91e927b47ed8
Anderson Marlon <anderson18.marlon@gmail.com>
2023-04-28 13:06:42 -0300

    First Release

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import styled from "styled-components";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <HomeDetails>
+      <img src="https://github.com/refined-github.png" alt="github" />
+      <h2>RunInLinux</h2>
+      <p>The website to know if the game runs or not in Linux</p>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Games</th>
+            <th>Developer</th>
+            <th>RunInLinux?</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Counter Strike: Global Offensive</td>
+            <td>Valve Software</td>
+            <td>✅</td>
+          </tr>
+
+          {/* ADD MORE GAMES HERE*/}
+          {/* <tr>
+            <td>Counter Strike: Global Offensive</td>
+            <td>Valve Software</td>
+            <td>✅</td>
+          </tr> */}
+        </tbody>
+      </table>
+    </HomeDetails>
   );
 }
+
+const HomeDetails = styled.div`
+  img {
+    width: 20%;
+  }
+
+  img,
+  h2 {
+    margin-bottom: -1.5rem;
+  }
+
+  table.table {
+    width: 100%;
+    background-color: #fff;
+    border-collapse: collapse;
+    border-width: 2px;
+    border-color: #ffcc00;
+    border-style: solid;
+    color: #000000;
+  }
+
+  table.table td,
+  table.table th {
+    border-width: 2px;
+    border-color: #181a1b;
+    border-style: solid;
+    padding: 1rem 5rem;
+  }
+
+  table.table thead {
+    background-color: #7159c1;
+    color: #fff;
+  }
+`;
 
 export default App;
